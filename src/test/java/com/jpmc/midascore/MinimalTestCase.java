2d27c9af7a4b908244e4c1ab5ded3c62b8a59e85
cajcrook <cajcrook@gmail.com>
2025-02-24 11:41:32 +0000

    task one - application boot - complete

---
+++ src/test/java/com/jpmc/midascore/MinimalTestCase.java
@@ -0,0 +1,13 @@
+package com.jpmc.midascore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidasCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test simply verifies if the Spring application context can start
+    }
+}
